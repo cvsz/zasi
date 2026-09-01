a65ed247f4211c34ec6019af026b3e3ffb61eb0e
cvsz <seaza@msn.com>
2026-09-01 07:55:07 +0000

    build: update distribution metadata to v10.0.0 and generate wheel packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zasi",
-    version="5.0.0",
+    version="10.0.0",
     description="Omniscient Sovereign Artificial Superintelligence (ASI) Architecture",
     packages=find_packages(),
     python_requires=">=3.9",
