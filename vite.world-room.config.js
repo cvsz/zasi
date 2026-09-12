75110069adab5fcf95102df2a5e5dd63e4fbef45
cvsz <seaza@msn.com>
2026-09-12 22:44:03 +0700

    fix(world-room): use existing Vite dependencies

--- vite.world-room.config.js
+++ vite.world-room.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: 'world-room',
-  plugins: [react()],
   server: {
     host: '127.0.0.1',
     port: 5174,
